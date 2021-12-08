ccb1d93c3ed6ea597dad91770790fe47c8844c0f
nax1016cs <51845490+nax1016cs@users.noreply.github.com>
2021-12-08 19:18:23 +0800

    Add files via upload

--- HW3/mmdetection/config.py
+++ HW3/mmdetection/config.py
@@ -1,8 +1,8 @@
 _base_ = 'configs/mask_rcnn/mask_rcnn_x101_32x4d_fpn_2x_coco.py'
 model = dict(
     roi_head=dict(
-        bbox_head = dict(num_classes=1),
-        mask_head = dict(num_classes=1)
+        bbox_head=dict(num_classes=1),
+        mask_head=dict(num_classes=1)
     ),
 )
 data_root = '/eva_data/ming/test/DRVL/HW3/'
@@ -10,22 +10,23 @@ dataset_type = 'COCODataset'
 classes = ('nuclei',)
 runner = dict(type='EpochBasedRunner', max_epochs=100)
 data = dict(
-    samples_per_gpu = 1,
-    workers_per_gpu = 1,
-    train = dict(
-        img_prefix = data_root + 'dataset/full_train/',
-        classes = classes,
-        ann_file = data_root + 'nuclei.json'
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        img_prefix=data_root + 'dataset/full_train/',
+        classes=classes,
+        ann_file=data_root + 'nuclei.json'
     ),
-    val = dict(
-        img_prefix = data_root + 'dataset/full_train/',
-        classes = classes,
-        ann_file = data_root + 'nuclei.json'
+    val=dict(
+        img_prefix=data_root + 'dataset/full_train/',
+        classes=classes,
+        ann_file=data_root + 'nuclei.json'
     ),
-    test = dict(
-        img_prefix = data_root +'dataset/test/',
-        classes = classes,
-        ann_file = data_root + 'test_img_ids.json'
+    test=dict(
+        img_prefix=data_root + 'dataset/test/',
+        classes=classes,
+        ann_file=data_root + 'test_img_ids.json'
     ),
 )
-load_from = 'ckpt/mask_rcnn_x101_32x4d_fpn_2x_coco_bbox_mAP-0.422__segm_mAP-0.378_20200506_004702-faef898c.pth'
+load_from = 'ckpt/mask_rcnn_x101_32x4d_fpn_2x_coco_bbox_mAP-0.422__segm_mAP\
+            -0.378_20200506_004702-faef898c.pth'
